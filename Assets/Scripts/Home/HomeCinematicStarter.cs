7ecdda182cb695d9c0c240be25444ebe18cf978f
Andrew Hung <awhhung@gmail.com>
2019-01-27 14:28:07 -0800

    make cinematic only start when entering home for first time

---
+++ Assets/Scripts/Home/HomeCinematicStarter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeCinematicStarter : MonoBehaviour
+{
+    private void Awake()
+    {
+        FindObjectOfType<CinematicPlayer>().PlayOpeningCinematicIfNecessary();
+    }
+}
