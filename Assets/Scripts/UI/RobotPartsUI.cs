2b4011c6d90bce8236d1408e935fb6e3c9edfecc
Andrew Hung <awhhung@gmail.com>
2019-01-27 00:48:04 -0800

    finished HUD prefab and robot parts diagram behaviour

---
+++ Assets/Scripts/UI/RobotPartsUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class RobotPartsUI : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject body;
+    public GameObject leftArm;
+    public GameObject rightArm;
+    public GameObject leftWheel;
+    public GameObject rightWheel;
+
+    public void FindHead()
+    {
+        head.SetActive(true);
+    }
+
+    public void FindBody()
+    {
+        body.SetActive(true);
+    }
+
+    public void FindLeftArm()
+    {
+        leftArm.SetActive(true);
+    }
+
+    public void FindRightArm()
+    {
+        rightArm.SetActive(true);
+    }
+
+    public void FindLeftWheel()
+    {
+        leftWheel.SetActive(true);
+    }
+
+    public void FindRightWheel()
+    {
+        rightWheel.SetActive(true);
+    }
+}
