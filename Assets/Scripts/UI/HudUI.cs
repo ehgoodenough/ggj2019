cf9daedbad40ddfaee6f5c184ab8118af68a12da
ackleyrc <ackleyrc@gmail.com>
2019-01-28 21:24:18 -0800

    Make Robot Parts and Battery UI only appear after exiting Title Screen

---
+++ Assets/Scripts/UI/HudUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudUI : MonoBehaviour
+{
+    public float startFadeDelay = 0.5f;
+    public float canvasGroupFadeDuration = 0.5f;
+    public CanvasGroup defaultCanvasGroup;
+
+    private void Awake()
+    {
+        if (defaultCanvasGroup) defaultCanvasGroup.alpha = 0f;
+        EventBus.Subscribe<ExitTitleScreenEvent>(OnExitTitleScreenEvent);
+    }
+
+    private void OnExitTitleScreenEvent(ExitTitleScreenEvent e)
+    {
+        StartCoroutine(RevealCanvasGroup(defaultCanvasGroup));
+    }
+
+    IEnumerator RevealCanvasGroup(CanvasGroup canvasGroup, float delay = 0f)
+    {
+        if (!canvasGroup) yield return null;
+
+        yield return new WaitForSeconds(delay);
+
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / canvasGroupFadeDuration;
+            yield return null;
+            while (canvasGroup == null)
+            {
+                yield return null;
+            }
+        }
+    }
+}
