802cf9de0ab35825934b09885ca17f27285b9974
ackleyrc <ackleyrc@gmail.com>
2019-01-30 12:01:06 -0800

    Add prompt at start telling player how to hold up photo again

---
+++ Assets/Scripts/UI/ShowPhotoPromptUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPhotoPromptUI : MonoBehaviour
+{
+    private CanvasGroup showPhotoPromptGroup;
+
+    private void Awake()
+    {
+        showPhotoPromptGroup = GetComponent<CanvasGroup>();
+        showPhotoPromptGroup.alpha = 0f;
+
+        EventBus.Subscribe<PhotoLoweredAtStartEvent>(OnPhotoLoweredAtStartEvent);
+    }
+
+    private void OnPhotoLoweredAtStartEvent(PhotoLoweredAtStartEvent e)
+    {
+        StartCoroutine(FadeInFadeOut(showPhotoPromptGroup, 0.2f, 1.2f, 3f, 1f));
+    }
+
+    IEnumerator FadeInFadeOut(CanvasGroup canvasGroup, float initialDelay, float fadeInDuration, float holdTime, float fadeOutDuration)
+    {
+        if (canvasGroup)
+        {
+            yield return new WaitForSeconds(initialDelay);
+
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / fadeInDuration;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(holdTime);
+
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / fadeOutDuration;
+                yield return null;
+            }
+        }
+    }
+}
