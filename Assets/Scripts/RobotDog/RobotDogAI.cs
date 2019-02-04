836537fb2e8e049258ebd65c7bee15772c83869b
ackleyrc <ackleyrc@gmail.com>
2019-02-04 09:25:09 -0800

    Refactor Dorg's footstep audio into independent script component

--- Assets/Scripts/RobotDog/RobotDogAI.cs
+++ Assets/Scripts/RobotDog/RobotDogAI.cs
@@ -10,23 +10,12 @@ public class RobotDogAI : MonoBehaviour
     public float updateDestinationFrequency = 1f;
     public float startFollowingHysteresis = 2f;
 
-    [FMODUnity.EventRef]
-    public string footstepEvent;
-    [FMODUnity.EventRef]
-    public string indoorFootstepEvent;
-    public float strideLength = 5.0f;
-
     private NavMeshAgent agent;
     private Vector3 nextPosition; // This will be the most recent position of the player
     private Vector3 previousPosition; // This will be the position before the most recent position of the player
     private float startFollowingDistance;
 
     private PoochAnimator animator;
-
-    [SerializeField]
-    private Vector3 lastFootstepLocation;
-    private bool muteFootsteps = false;
-    private bool outside = false;
 
     private static bool chairHasBeenPickedUp = false;
     private static bool artworkHasBeenPickedUp = false;
@@ -50,11 +39,7 @@ public class RobotDogAI : MonoBehaviour
         // Debug.Log("Player: " + player);
         // Debug.Log("PlayerMovement: " + FindObjectOfType<PlayerMovement>());
         if (player == null) player = FindObjectOfType<PlayerMovement>().gameObject;
-
-        EventBus.Subscribe<EnterHomeEvent>(OnEnterHomeEvent);
-        EventBus.Subscribe<EnterCityEvent>(OnEnterCityEvent);
-        EventBus.Subscribe<ExitHomeEvent>(OnExitHomeEvent);
-        EventBus.Subscribe<ExitCityEvent>(OnExitCityEvent);
+        
         EventBus.Subscribe<ObjectiveItemPickedUpEvent>(OnObjectiveItemHasBeenPickedUpEvent);
     }
 
@@ -70,7 +55,7 @@ public class RobotDogAI : MonoBehaviour
     void Update()
     {
         // Change Animation state according to current speed
-        float currentSpeed = agent.velocity.magnitude;
+        float currentSpeed = GetCurrentSpeed();
         float currentSpeedNormalized = currentSpeed / agent.speed;
         // Debug.Log("Current Speed Normalized: " + currentSpeedNormalized);
         if (currentSpeedNormalized > 0.75f )
@@ -85,11 +70,11 @@ public class RobotDogAI : MonoBehaviour
         {
             animator.Idle();
         }
-
-        if (!muteFootsteps && currentSpeed > 0 && Vector3.Distance(lastFootstepLocation, transform.position) > strideLength)
-        {
-            PlayFootstep();
-        }
+    }
+    
+    public float GetCurrentSpeed()
+    {
+        return agent.velocity.magnitude;
     }
 
     IEnumerator FollowPlayer()
@@ -114,12 +99,11 @@ public class RobotDogAI : MonoBehaviour
         return positionFound ? hit.position : this.transform.position; // If cannot find position, stay put
     }
 
-    private void OnDrawGizmosSelected()
+    private void PlaceOnNavMesh(Vector3 position)
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawCube(previousPosition, Vector3.one * 0.5f);
-        Gizmos.color = Color.green;
-        Gizmos.DrawCube(nextPosition, Vector3.one * 0.5f);
+        NavMeshHit hit;
+        bool positionFound = NavMesh.SamplePosition(position, out hit, 2.5f, 1);
+        this.transform.position = positionFound ? hit.position : this.transform.position; // If cannot find position, stay put
     }
 
     private void OnObjectiveItemHasBeenPickedUpEvent(ObjectiveItemPickedUpEvent e)
@@ -143,54 +127,11 @@ public class RobotDogAI : MonoBehaviour
         }
     }
 
-    private void OnEnterHomeEvent(EnterHomeEvent e)
+    private void OnDrawGizmosSelected()
     {
-        // Debug.Log("RobotDogAI.OnEnterHomeEvent()");
-        // Debug.Log("Robot Dog Position: " + this.transform.position);
-        // PlaceOnNavMesh(e.homeState.dogStart.position);
-        // this.transform.rotation = e.homeState.dogStart.rotation;
-        // Debug.Log("Position: " + this.transform.position);
-        muteFootsteps = false;
-        outside = false;
-    }
-
-    private void OnExitHomeEvent(ExitHomeEvent e)
-    {
-        muteFootsteps = true;
-    }
-
-    private void OnEnterCityEvent(EnterCityEvent e)
-    {
-        // Debug.Log("RobotDogAI.OnEnterCityEvent()");
-        // Debug.Log("Robot Dog Position: " + this.transform.position);
-        // PlaceOnNavMesh(e.cityState.dogStart.position);
-        // this.transform.rotation = e.cityState.dogStart.rotation;
-        muteFootsteps = false;
-        outside = true;
-    }
-
-    private void OnExitCityEvent(ExitCityEvent e)
-    {
-        muteFootsteps = true;
-    }
-
-    private void PlaceOnNavMesh(Vector3 position)
-    {
-        NavMeshHit hit;
-        bool positionFound = NavMesh.SamplePosition(position, out hit, 2.5f, 1);
-        this.transform.position = positionFound ? hit.position : this.transform.position; // If cannot find position, stay put
-    }
-
-    private void PlayFootstep()
-    {
-        lastFootstepLocation = transform.position;
-        if (outside)
-        {
-            FMODUnity.RuntimeManager.PlayOneShot(footstepEvent, transform.position);
-        }
-        else
-        {
-            FMODUnity.RuntimeManager.PlayOneShot(indoorFootstepEvent, transform.position);
-        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(previousPosition, Vector3.one * 0.5f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(nextPosition, Vector3.one * 0.5f);
     }
 }
---
+++ Assets/Scripts/RobotDog/RobotDogAudio.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotDogAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string footstepEvent;
+    [FMODUnity.EventRef]
+    public string indoorFootstepEvent;
+    public float strideLength = 2.0f;
+
+    [SerializeField]
+    private Vector3 lastFootstepLocation;
+    private bool muteFootsteps = false;
+    private bool outside = false;
+
+    private RobotDogAI dogAI;
+
+    private void Awake()
+    {
+        dogAI = GetComponent<RobotDogAI>();
+
+        EventBus.Subscribe<EnterHomeEvent>(OnEnterHomeEvent);
+        EventBus.Subscribe<EnterCityEvent>(OnEnterCityEvent);
+        EventBus.Subscribe<ExitHomeEvent>(OnExitHomeEvent);
+        EventBus.Subscribe<ExitCityEvent>(OnExitCityEvent);
+    }
+
+    private void Update()
+    {
+        if (!muteFootsteps && dogAI.GetCurrentSpeed() > 0 && Vector3.Distance(lastFootstepLocation, transform.position) > strideLength)
+        {
+            PlayFootstep();
+        }
+    }
+
+    private void PlayFootstep()
+    {
+        lastFootstepLocation = transform.position;
+        if (outside)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(footstepEvent, transform.position);
+        }
+        else
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(indoorFootstepEvent, transform.position);
+        }
+    }
+
+    private void OnEnterHomeEvent(EnterHomeEvent e)
+    {
+        // Debug.Log("RobotDogAudio.OnEnterHomeEvent()");
+        muteFootsteps = false;
+        outside = false;
+    }
+
+    private void OnExitHomeEvent(ExitHomeEvent e)
+    {
+        muteFootsteps = true;
+    }
+
+    private void OnEnterCityEvent(EnterCityEvent e)
+    {
+        // Debug.Log("RobotDogAudio.OnEnterCityEvent()");
+        muteFootsteps = false;
+        outside = true;
+    }
+
+    private void OnExitCityEvent(ExitCityEvent e)
+    {
+        muteFootsteps = true;
+    }
+}
