85c56edc200258c5660b0b0050c91f5e8d91fbdc
ackleyrc <ackleyrc@gmail.com>
2019-02-04 00:09:52 -0800

    WIP: Try to get painting to appear to float on water

---
+++ Assets/Scripts/Effects/FloatOnWaterEffect.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatOnWaterEffect : MonoBehaviour
+{
+    public float waterYPosition;
+    public Transform floatingTransform;
+    public Oscillate oscillate;
+
+    private bool isOnWater;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        // Debug.Log("rb: " + rb);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            Debug.Log("Enter collision with Water");
+
+            // Set this object to be level with the water
+            Vector3 pos = floatingTransform.position;
+            Debug.Log("Other Y Position: " + other.transform.position.y);
+            pos.y = waterYPosition; // other.transform.position.y;
+            floatingTransform.position = pos;
+            floatingTransform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            rb.useGravity = false;
+
+            // oscillate.Initialize();
+            oscillate.oscillateOn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            Debug.Log("Exit collision with Water");
+            // rb.constraints = RigidbodyConstraints.None;
+            oscillate.oscillateOn = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Effects/Oscillate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillate : MonoBehaviour
+{
+    public bool oscillateOn = false;
+
+    public Vector3 amplitude = Vector3.zero;
+    public Vector3 rate = Vector3.one;
+
+    public bool randPosOffset = false;
+    public Vector3 posOffset = Vector3.zero;
+
+    public bool randTimeOffset = false;
+    public Vector3 timeOffset = Vector3.zero;
+
+    private Vector3 defaultPos = Vector3.zero;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+    
+    /*
+    private void OnValidate()
+    {
+        Initialize(); // This ensures that values are re-initialized when object edited in inspector
+    }
+    */
+
+    public void Initialize()
+    {
+        defaultPos = transform.localPosition;
+
+        if (randTimeOffset)
+        {
+            timeOffset = Random.insideUnitSphere * Mathf.PI;
+        }
+
+        if (randPosOffset)
+        {
+            posOffset = Random.insideUnitSphere * Mathf.PI;
+        }
+    }
+
+    void Update()
+    {
+        if (oscillateOn)
+        {
+            Vector3 pos = transform.localPosition;
+
+            float xPos = (defaultPos.x + posOffset.x) + amplitude.x * Mathf.Sin((Time.time + timeOffset.x) * rate.x);
+            float yPos = (defaultPos.y + posOffset.y) + amplitude.y * Mathf.Sin((Time.time + timeOffset.y) * rate.y);
+            float zPos = (defaultPos.z + posOffset.z) + amplitude.z * Mathf.Sin((Time.time + timeOffset.z) * rate.z);
+
+            transform.localPosition = new Vector3(xPos, yPos, zPos);
+        }
+    }
+}
