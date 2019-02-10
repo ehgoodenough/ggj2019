8bda758f2bdd0bd4cd6ae45f4908733f6bbfa4dc
ackleyrc <ackleyrc@gmail.com>
2019-02-09 20:49:29 -0800

    Parent final cinematic objects under one parent with toggle for playing on Awake

---
+++ Assets/Scripts/Home/FinalCinematicStarter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class FinalCinematicStarter : MonoBehaviour
+{
+    public bool playOnAwake;
+    public PlayableDirector finalCinematicDirector;
+    public Camera finalCinematicCamera;
+    public GameObject friendPiecesApart;
+    public GameObject assemblyParticleEffect;
+    public GameObject cinemachineVirtualCamera;
+
+    private void Awake()
+    {
+        if (playOnAwake)
+        {
+            PlayFinalCinematic();
+        }
+        else
+        {
+            finalCinematicCamera.gameObject.SetActive(false);
+            friendPiecesApart.SetActive(false);
+            assemblyParticleEffect.SetActive(false);
+            cinemachineVirtualCamera.SetActive(false);
+        }
+    }
+
+    public void PlayFinalCinematic()
+    {
+        friendPiecesApart.SetActive(true);
+        assemblyParticleEffect.SetActive(true);
+        cinemachineVirtualCamera.SetActive(true);
+        finalCinematicCamera.gameObject.SetActive(true);
+        finalCinematicDirector.Play();
+    }
+}
