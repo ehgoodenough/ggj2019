735a78354880e5479f9497af61b9e585904fa7ae
NickNamelous <guerillarenegade@yahoo.com>
2019-02-21 06:16:44 -0800

    Experimenting with a Title Screen-Attract mode
    Experimenting with a Title Screen-Attract mode
    -View in RobertTitleScreen2 scene.
    -Experimenting with different text (viewable variations in the Canvas)

---
+++ Assets/Scripts/Camera/RainFollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainFollowCamera : MonoBehaviour
+{
+    public Transform track;
+    
+    void Update()
+    {
+        transform.position = track.position;
+    }
+}
---
+++ Assets/Scripts/UI/StartPrompt_FadeText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPrompt_FadeText : MonoBehaviour
+{
+    public float titleFadeDuration;
+    public CanvasGroup startPromptCanvas;
+
+
+    void Awake()
+    {
+        // EventBus.PublishEvent(new TitleScreenStartEvent(playerObj));
+        startPromptCanvas.alpha = 0;
+        StartCoroutine(RevealCanvas(startPromptCanvas, 5.5f));
+    }
+
+    IEnumerator RevealCanvas(CanvasGroup canvas, float delay = 0f)
+    {
+        yield return new WaitForSeconds(delay);
+
+        while (canvas && canvas.alpha < 1)
+        {
+            canvas.alpha += Time.deltaTime / titleFadeDuration;
+            yield return null;
+            while (canvas == null)
+            {
+                yield return null;
+            }
+        }
+        StartCoroutine(FadeOut(startPromptCanvas, 2f));
+    }
+
+        IEnumerator FadeOut(CanvasGroup canvas, float delay = 0f)
+    {
+        yield return new WaitForSeconds(delay);
+
+        while (canvas && canvas.alpha > 0)
+        {
+            canvas.alpha -= Time.deltaTime / titleFadeDuration;
+            yield return null;
+            while (canvas == null)
+            {
+                yield return null;
+            }
+        }
+        StartCoroutine(RevealCanvas(startPromptCanvas, 0f));
+    }
+}
